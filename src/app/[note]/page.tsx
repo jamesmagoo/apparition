60f89b165f53a78dceeae84acd210ff34d0175b7
James McGauran <jamesmcgauran11@gmail.com>
2023-08-16 11:04:55 +0100

    Make branch to hold three js stuff

--- src/app/[note]/page.tsx
+++ src/app/[note]/page.tsx
@@ -11,23 +11,8 @@ interface Props {
 export default async function NotePage({content, created_at, hashtags} : Props){
 
     return (
-        <div>
-         {/* <p className="text-body5 text-gray-400">
-            {new Date(created_at * 1000).toISOString().split("T")[0]}
-          </p> */}
+    <div> 
         <p>HHHHHHH</p>
-        {/* <ul className="flex flex-wrap gap-12">
-        {hashtags
-          .filter((t) => hashtags.indexOf(t) === hashtags.lastIndexOf(t))
-          .map((hashtag) => (
-            <li§
-              key={hashtag}
-              className="bg-gray-300 text-body5 text-gray-900 font-medium rounded-24 px-12 py-4"
-            >
-              #{hashtag}
-            </li§>
-          ))}
-      </ul> */}
     </div>
     )
 }
