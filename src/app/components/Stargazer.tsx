de5213e6dd0bf89466214620413c1fed25918c16
James McGauran <jamesmcgauran11@gmail.com>
2023-08-16 18:02:08 +0100

    Add react-force-graph, remove reagraph

--- src/app/components/Stargazer.tsx
+++ src/app/components/Stargazer.tsx
@@ -1,16 +1,63 @@
-import { useRef, useState } from 'react'
-import { Mesh } from 'three'
+import { ForceGraph3D } from "react-force-graph"
 
 type Props = {}
-
 const Stargazer = (props: Props) => {
 
-   
+    const notesData = {
+        "nodes": [ 
+            { 
+              "id": "id1",
+              "name": "name1",
+              "val": 1 ,
+              "others" : {
+                "note" : "sdasdsadasdas"
+              }
+            },
+            { 
+              "id": "id2",
+              "name": "name2",
+              "val": 10 
+            },
+        ],
+        "links": [
+            {
+                "source": "id1",
+                "target": "id2"
+            },
+        ]
+    }
+
+    function genRandomTree(N = 50, reverse = false) {
+        return {
+          nodes: [...Array(N).keys()].map(i => ({ id: i })),
+            links: [...Array(N).keys()]
+          .filter(id => id)
+          .map(id => ({
+            [reverse ? 'target' : 'source']: id,
+            [reverse ? 'source' : 'target']: Math.round(Math.random() * (id-1))
+          }))
+        };
+      }
+      function genRandomTreeNoLinks(N = 50, reverse = false) {
+        return {
+          nodes: [...Array(N).keys()].map(i => ({ id: i })),
+            links: [4]
+          .filter(id => id)
+          .map(id => ({
+            [reverse ? 'target' : 'source']: id,
+            [reverse ? 'source' : 'target']: Math.round(Math.random() * (id-1))
+          }))
+        };
+      }
 
     return (
-        <div className='canvas-container'>
-        <p>New approach</p>
-        </div>
+       <ForceGraph3D 
+       graphData={genRandomTreeNoLinks(25,false)}
+       width={1200}
+       height={800}
+       backgroundColor={"black"}
+       onNodeClick={(node)=>{console.log(`here we go : ${node.name}`)}}
+       onNodeHover={(node)=>{console.log(`here we go : ${node?.other?.note}`)}}/>
     )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,8 @@
 'use client'
-import Stargazer from './components/Stargazer';
-import { GraphCanvas } from 'reagraph';
-import { theme1 } from './graph/theme';
 
-const nodes = [
-  {
-    id: '1',
-    label: '1'
-  },
-  {
-    id: '2',
-    label: '2'
-  }
-];
+import Stargazer from "./components/Stargazer";
 
-const edges = [
-  {
-    source: '1',
-    target: '2',
-    id: '1-2',
-    label: '1-2'
-  },
-  {
-    source: '2',
-    target: '1',
-    id: '2-1',
-    label: '2-1'
-  }
-];
+
 
 /**
  * 
@@ -42,15 +17,8 @@ export default function Home() {
   }
 
   return (
-    // <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-blue-100 h-max">
-    <div style={{ width: '600px', height: '400px' }}>
-      <GraphCanvas
-        theme={theme1}
-        nodes={nodes}
-        edges={edges}
-        onCanvasClick={()=>console.log("click")}
-        onNodeClick={(node)=>{console.log(`clinking this node ${node.id}`)}}
-      />
+    <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-blue-100 h-max">
+      <Stargazer/>
       <h6>Not in a million years</h6>
     </div>
   )
